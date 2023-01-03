2e736c72054fcf9cd1969d07b05f1823f3d3b3da
Nick Wylynko <nick@wylynko.com>
2023-01-03 11:57:18 +0800

    add a little setup for the dev environment

--- scripts.ts
+++ scripts.ts
@@ -1,8 +1,14 @@
-import { scripts } from "scriptful"
+import { scripts, command as c, variants } from "scriptful"
 
 export default scripts({
-  "build": "tsc",
-  "dev": "tsc -w",
-  "test": "vitest",
-  "release": "bumpp"
+  "build": c({ run: "tsc" }, "Build the project"),
+  "dev": c({ run: "tsc -w" }, "Rebuild the project on file change"),
+  "setup": variants({
+    "dev": c({ run: "pnpm link ." }, "Make the package available")
+  }),
+  "test": c({ run: "vitest" }, "Run the tests"),
+  "workflow": variants({
+    "test": c({ run: "act -j run-tests" }, "Run the github action 'run-tests' using docker https://github.com/nektos/act"),
+  }),
+  "release": c({ run: "bumpp" }, "Bump the version (and github action will publish it)"),
 })
